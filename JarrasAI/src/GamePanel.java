126e2ca478f06573b03ee983180f40fe87172990
bitcod3r <i.am@bitcoder.net>
2014-07-24 12:10:13 -0500

    Pruebas para mostrar imagen de fondo

--- JarrasAI/src/GamePanel.java
+++ JarrasAI/src/GamePanel.java
@@ -1,8 +1,11 @@
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
-import java.awt.Toolkit;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
 
@@ -14,7 +17,27 @@ public class GamePanel extends JPanel {
 		setBounds(14, 5, 340, 310);
 	}
 	
+	public void paintComponent(Graphics g) {
 
+		Image imagen;
+		try {
+			imagen = ImageIO.read(     new File("robot.jpg")      );
+			g.drawImage(imagen, 0, 0, null);
+		} catch (IOException e) {
+			//e.printStackTrace();
+			g.setColor(Color.GREEN);
+			g.fillOval(0, 0, 10, 10);
+		}
+        
+//		Toolkit tk = Toolkit.getDefaultToolkit();
+//		imagen = tk.createImage("robot.jpg");
+//		g.drawImage(imagen, 0, 0, null);
+		//g.fillOval(x, y, size, size);
+		
+		g.setColor(Color.GREEN);
+		g.fillOval(0, 0, 10, 10);
+		
+	}
 	
 
 	
